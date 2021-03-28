f3f0da034c46b90256f158d601b005c83de15255
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-28 21:16:30 +0900

    UPDATE <main.go> variables and constants

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github/jeongjiwoo0522/scrapperWithGo/something"
 )
 
 func main() {
+	fmt.Println("Hello World")
 	something.SayHello()
+
+	var name string = "Cat" //variable
+	const age int = 12 // constant
+	var id, address string
+	var (
+		height int
+		weight float32
+	)
+	short := "hello"
+	fmt.Println(name, id, address, height, weight, short)
+	// variables and constants
 }
