2c1fac38c594b777e4aba9a3b7d5735d7cd7b1ca
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-03 14:04:13 +0900

    [ADD] <account/account.go> Contructor Pattern

---
+++ account/account.go
@@ -0,0 +1,13 @@
+package account
+
+// Account structure
+type Account struct {
+	owner   string
+	balance int
+}
+
+// NewAccount create Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
--- main.go
+++ main.go
@@ -2,27 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github/jeongjiwoo0522/scrapperWithGo/something"
+	"github/jeongjiwoo0522/scrapperWithGo/account"
 )
 
-func multiply(a int, b int) int {
-	return a * b
-}
-
 func main() {
-	fmt.Println("Hello World")
-	something.SayHello()
-
-	var name string = "Cat" //variable
-	const age int = 12 // constant
-	var id, address string
-	var (
-		height int
-		weight float32
-	)
-	short := "hello"
-	fmt.Println(name, id, address, height, weight, short)
-	// variables and constants
-
-	fmt.Println(multiply(2, 3))
+	account := account.NewAccount("nico")
+	fmt.Println(*account)
 }
