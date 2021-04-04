d1aa2f6f79e1f649c79971ebc6940d8599ca88b3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 21:13:25 +0900

    [MODIFY] main.go goroutine channel

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github/jeongjiwoo0522/scrapperWithGo/dict"
+	"net/http"
 )
 
+type requestResult struct {
+	url string
+	status string
+}
+
+var errRequestFailed error = errors.New("Request Failed")
+
 func main() {
-	dictionary := dict.Dictionary{}
-	word := "hello"
-	dictionary.Add(word, "First")
-	dictionary.Update(word, "Second")
-	dictionary.Delete(word)
-	definition, err := dictionary.Search(word)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	results := make(map[string]string)
+	urls := []string{
+		"https://github.com/",
+		"https://nomadcoders.co/",
+		"https://www.google.com/",
+		"https://www.naver.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://www.amazon.com/",
 	}
+
+	c := make(chan requestResult)
+	for _, url := range urls {
+		go hitUrl(url, c)
+	}
+
+	for i:=0; i<len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitUrl(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
 }
