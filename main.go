e50757d711e01bf5438feb52904f8d3f4b789d19
Jiwoo <63524088+jeongjiwoo0522@users.noreply.github.com>
2021-04-06 15:40:36 +0900

    [MODIFY] <main.go> file download

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"github.com/labstack/echo"
-	"net/http"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World!")
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	return nil
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
 	e.Logger.Fatal(e.Start(":1323"))
 }
